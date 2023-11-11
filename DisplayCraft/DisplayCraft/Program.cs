eaacfe2039a2e8fb80853f9164cc1ace0c7a8da9
LimaDiogenes <you@example.comdiogenesdopradolima@gmail.com>
2023-11-10 22:09:48 -0300

    101123 - in progress

--- DisplayCraft/DisplayCraft/Program.cs
+++ DisplayCraft/DisplayCraft/Program.cs
@@ -1,3 +1,4 @@
+using static DisplayCraft.Display;
 
 namespace DisplayCraft
 {
@@ -5,8 +6,13 @@ namespace DisplayCraft
     {
         static void Main(string[] args)
         {
-            Display d1 = new Display();
-            
+ 
+                Display d1 = new Display(width:50, height: 15, posX: 5, posY: 5, style: BorderStyle.Curved, background:ConsoleColor.Blue);
+                d1.Borders();
+
+                Display d2 = new(width: 20, height: 5, posX: 7, posY: 7 );  
+                d2.Borders(); 
+             
         }
 
     }
